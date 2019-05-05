fec1d888259b9b13ff53e81008585bfe3dd201e0
srius1977 <minameissri@gmail.com>
2019-05-05 02:01:06 -0400

    Update new.js

--- static/js/new.js
+++ static/js/new.js
@@ -2,3 +2,11 @@ function jsfunction() {
   var myHeading = document.querySelector('h1');
   myHeading.textContent = 'Hello world!';
 }
+
+var myButton=document.querySelector('button')
+var myHeading=document.querySelector('h1')
+
+function setUserName() {
+  var myName=prompt('Please enter your name')
+  localStorage.setItem('name', myName)
+}
